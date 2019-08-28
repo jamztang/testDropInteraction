bd6bafbe99ea680f6402efad45a8c10eec790b3c
James Tang <james@goodnotesapp.com>
2019-08-28 17:33:10 +0800

    Added UIDropInteraction

--- testDropInteraction/ViewController.swift
+++ testDropInteraction/ViewController.swift
@@ -10,11 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var dropInteraction: UIDropInteraction!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+        dropInteraction = UIDropInteraction(delegate: self)
+        view.addInteraction(dropInteraction)
+    }
+}
+
+extension ViewController: UIDropInteractionDelegate {
+    func dropInteraction(_ interaction: UIDropInteraction, canHandle session: UIDropSession) -> Bool {
+        Swift.print("dropInteraction canHandle \(session)")
+        return true
     }
 
+    func dropInteraction(_ interaction: UIDropInteraction, sessionDidUpdate session: UIDropSession) -> UIDropProposal {
+        // If a drag comes in, we copy the file. We don't want to consume it.
+        return UIDropProposal(operation: .copy)
+    }
 
+    func dropInteraction(_ interaction: UIDropInteraction, performDrop session: UIDropSession) {
+        Swift.print("dropInteraction performDrop \(session)")
+    }
 }
 
