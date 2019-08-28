50a2efe27c12a899577a294c817b4975da438092
James Tang <james@goodnotesapp.com>
2019-08-28 16:39:06 +0800

    Initial Commit

---
+++ testDropInteraction/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testDropInteraction
+//
+//  Created by James Tang on 28/8/2019.
+//  Copyright © 2019 James Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
